eaeb2ad2b9dd274d589970cfea5264676f9c639a
kek <kek@localhost>
2012-11-05 11:25:58 +0100

    test

--- public/js/overlay.js
+++ public/js/overlay.js
@@ -1,31 +1,31 @@
 
 
 $(document).ready(function() {
-	$('#bt_fermer').click(function() {
-			$('#overlay').hide();
-			$('#bglayer').hide();
-	});
-	$('#bt_co_layout').click(function() {
-		$('#overlay').show();
-		$('#bglayer').show();
-	});
-	$('#input_psw').keypress(function(e) {
-		if(e.which==13)
-			verifConnexion();
-	});
-        $('#overlay').css('top',screen.height /2);
-        $('#overlay').css('left',screen.width /2);
+    $('#bt_fermer').click(function() {
+                    $('#overlay').hide();
+                    $('#bglayer').hide();
+    });
+    $('#bt_co_layout').click(function() {
+            $('#overlay').show();
+            $('#bglayer').show();
+    });
+    $('#input_psw').keypress(function(e) {
+            if(e.which==13)
+                    verifConnexion();
+    });
+    $('#overlay').css('margin','auto');
+    $('#overlay').css('margin-top','10%');
 });
 
 function verifConnexion() {
-	var username = $('#input_user').val();
-	var mdp = $('#input_psw').val();
-	if((username == "") || (mdp == ""))
-        {
-		$('#err').after('<br><span id="erreur_co">Vous n\'avez pas remplis tous les champs<span><br>');
-        }
-        else
-	{	
-            $("#form_login").submit();  
-        }
+    var username = $('#input_user').val();
+    var mdp = $('#input_psw').val();
+    if((username == "") || (mdp == ""))
+    {
+        $('#err').after('<br><span id="erreur_co">Vous n\'avez pas remplis tous les champs<span><br>');
+    }
+    else
+    {	
+        $("#form_login").submit();  
+    }
 }	
