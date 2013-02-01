7f87d2e4d378d33c673dda389b8ac5e4474e3fef
kevin-verschaeve <kevin.verschaeve@live.fr>
2013-02-01 11:18:18 +0100

    coms service commercial, exploitation

--- applications/controllers/ServicecommercialController.php
+++ applications/controllers/ServicecommercialController.php
@@ -1,25 +1,61 @@
 <?php
+/**
+ * ContrÃ´leur de service commercial
+ * 
+ * PHP version 5
+ * 
+ * @category INSSET
+ * @package  Airline
+ * @author   Kevin Verschaeve <kevin.verschaeve@live.fr>
+ * @license  http://opensource.org/licenses/gpl-license.php GNU Public License
+ * @link     /servicecommercial
+ */
+
+/**
+ * Classe du contrÃ´leur service commercial
+ * 
+ * @category INSSET
+ * @package  Airline
+ * @author   Kevin Verschaeve <kevin.verschaeve@live.fr>
+ * @license  http://opensource.org/licenses/gpl-license.php GNU Public License
+ * @link     /servicecommercial
+ */
 class ServiceCommercialController extends Zend_Controller_Action
 {
+     /**
+	 * MÃ©thode d'initialisation du contrÃ´leur.
+	 * Permet de dÃ©clarer les css & js Ã  utiliser.
+	 * Verifie que l'on est connectÃ© 
+     * Verifie que l'on a les droits nÃ©cÃ©ssaires pour accÃ©der a la page
+	 * @return null
+	 */
     public function init()
     {
         $this->headStyleScript = array(
             'css' => 'service_commercial'
         );
-        if(!session_encours())
-        {
+        if (!session_encours()) {
             $redirector = $this->_helper->getHelper('Redirector');
             $redirector->gotoUrl($this->view->baseUrl());  
         }
-        if(!Services_verifAcces('Service commercial')) 
-        {
+        if (!Services_verifAcces('Service commercial')) {
             throw new Zend_Controller_Action_Exception('',403);
         }
     }
+    /**
+	 * Action index. Renvoi automatiquement vers l'action volscatalogue
+	 * 
+	 * @return null
+	 */
     public function indexAction() 
     {
         $this->_helper->actionStack('header','index','default',array('head' => $this->headStyleScript));
     }
+    /**
+    * Renvoi la liste des places rÃ©servÃ©es et libre pour un vol et par classe
+    *
+    * @return array
+    */
     public function placesbloqueesAction()
     {
         $this->_helper->actionStack('header','index','default',array('head' => $this->headStyleScript));
@@ -33,16 +69,13 @@ class ServiceCommercialController extends Zend_Controller_Action
         $tableContenir = new Table_Contenir;
         $lesResasVol = array();
         $nbPlacesReservees = array();
-        foreach($lesVols as $unVol)
-        {
+        foreach ($lesVols as $unVol) {
             $newVols[$unVol['idVol']] = $unVol;
             $lesResasVol[$unVol['idVol']] = $tableResa->getResasParVol($unVol['idVol']); 
             $lesMatriculesAvion[$unVol['idVol']] = $unVol['matriculeAvion'];
         }
-        foreach ($lesResasVol as $idVol => $uneResaVol)
-        {
-            foreach ($uneResaVol as $idUneResa)
-            {
+        foreach ($lesResasVol as $idVol => $uneResaVol) {
+            foreach ($uneResaVol as $idUneResa) {
                 $modeleAvion = $tableAvion->getModele($lesMatriculesAvion[$idVol]);
                 $laClasse = $tableResa->getClasse($idUneResa);
                 $placesTotales = $tableContenir->getNbPlacesTotales($modeleAvion, $laClasse['idClasse']);
